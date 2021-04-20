9c1fd05b1267c3ad948e68390ba1cf451a88df9e
jundai-muscle <60641016+jundai-muscle@users.noreply.github.com>
2021-04-20 21:22:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,97 +1,105 @@
-new Vue({
-    el:'#game',
-    data:{
-        card1:'',card2:'',card3:'',card4:'',
-        card5:'',card6:'',card7:'',card8:'',
-        card9:'',card10:'',card11:'',card12:'',
-        card13:'',card14:'',card15:'',card16:'',
-        number:[],
-        len:8,
-        pair1:'',
-        pair2:'',
-        set:undefined
-    },
-    created: function(){
-        function shuffle(num){
-            const n=num.length;
-            for(let i=n-1;i>0;i--){
-                let r=Math.floor(Math.random()*(i+1));
-                [num[i],num[r]]=[num[r],num[i]];
-            }
-            return num;
-        }
-        
-        let that=this;
-        for(let i=0;i<2;i++){
-            for(let j=1;j<=that.len;j++){
-                that.number.push(j);
-            }
-        }
+'use strict';
+let len=16;
+const ng=document.getElementById('new-game');
+let omote=0;
+let player=0;
+var player1score=0;
+var player2score=0;
+var set;
 
-        this.number=shuffle(this.number);
-    },
-    methods:{
-        onclick:function(e){
-            if(this.pair1!==""&&this.pair2!==""){
-                return;
-            }
-            function checked(that){
-                function faild(){
-                    console.log(e);
-                    console.log(that.set.id);
-                    e.target.textContent="";
-                    that.set.textContent="";
-                    that.pair1="";
-                    that.pair2="";
-                    that.set=undefined;
-                    return;
-                }
-                function match(){
-                    document.getElementById(e.target.id).style.border="none";
-                    document.getElementById(that.set.id).style.border="none";
-                    e.target.textContent="";
-                    that.set.textContent="";
-                    let Id=e.target.id;
-                    let Id2=that.set.id;
-                    let index=Number(Id.substr(Id.indexOf('-')+1))-1;
-                    let index2=Number(Id2.substr(Id2.indexOf('-')+1))-1;
-                    that.number[index]="";
-                    that.number[index2]="";
-                    
-                    that.pair1="";
-                    that.pair2="";
-                    that.set=undefined;
-                    return;
-                }
-                if(that.pair1!==that.pair2){
-                    return new Promise(resolve=>{
-                        setTimeout(()=>{resolve(faild());},500);
-                    })
-                }else{
-                    return new Promise(resolve=>{
-                        setTimeout(()=>{resolve(match());},500);
-                    })
-                }
-            }
-            async function check(that){
-                await checked(that)
-                return;
-            }
 
-            let Id=e.target.id;
-            let index=Number(Id.substr(Id.indexOf('-')+1))-1;
-            e.target.textContent=this.number[index];
-            
-            if(this.set===undefined){
-                this.set=e.target;
-            }
-            if(this.pair1===""){
-                this.pair1=this.number[index];
+
+ng.addEventListener('click',()=>{
+    ng.disabled=true;
+    dealCards()
+    .then(v=>{
+        console.log(v);
+        for(let i=1;i<=16;i++){
+            document.getElementsByClassName('card')[i-1].textContent=v[i-1];
+            //document.getElementsByClassName('card')[i-1].style.visibility="hidden";
+            document.getElementsByClassName('card')[i-1].style.opacity="0";
+            document.getElementsByClassName('field')[i-1].style.border="solid 1px black"
+            document.getElementById('cell-'+i).classList.add('back-card');
+            document.getElementById('player2').style.opacity="0.5";
+        }
+        document.getElementById('player-1-score').textContent=0;
+        document.getElementById('player-2-score').textContent=0;
+    })
+    .then(()=>{ng.disabled=false;})
+    .catch(()=>{ng.disabled=false;})
+})
+
+
+
+
+function checked(set,cell){
+    function checkout(){
+        if(cell.textContent===set.textContent){
+            cell.textContent="";
+            set.textContent="";
+            cell.parentNode.style.border="none";
+            set.parentNode.style.border="none";
+            //set.style.visibility="hidden";
+            //set.style.visibility="hidden";
+            omote=0
+            set=undefined;
+            if(player===0){
+                player1score++;
+                document.getElementById('player-1-score').textContent=player1score;
             }else{
-                this.pair2=this.number[index];
-                check(this);
+                player2score++;
+                document.getElementById('player-2-score').textContent=player2score;
             }
-
+        }else{
+            cell.style.opacity="0";
+            set.style.opacity="0";
+            set.classList.remove("card-"+set.textContent.toLowerCase());
+            set.classList.add("card-back");
+            omote=0
+            set=undefined;
+            if(player===0){
+                player=1;
+                document.getElementById('player1').style.opacity="0.5";
+                document.getElementById('player2').style.opacity="1.0";
+            }else{
+                document.getElementById('player2').style.opacity="0.5";
+                document.getElementById('player1').style.opacity="1.0";
+                player=0;
+            }
+            }
         }
-    }
+        return new Promise(resolve=>{
+            setTimeout(()=>{resolve(checkout());},1000);
+        })
+}
+
+async function check(cell,set){
+    const result=await checked(cell,set)
+    return;
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+	function cardClick(e){
+        let get=e.target;
+        if(omote===2 || get.textContent===""){
+            return;
+        }
+        //let index=Number(e.target.id.substr(e.target.id.indexOf('-')+1))-1;
+        if(omote===0){
+            get.classList.remove("card-back");
+            get.classList.add('card-'+get.textContent.toLowerCase());
+            get.style.opacity="1.0";
+            set=get;
+            omote=1;
+        }else{
+            omote++;
+            get.style.opacity="1.0";
+            check(set,e.target)
+        }
+	}
+	const card = document.getElementsByClassName('card');
+	for(let i = 0; i < 16; i++) {
+    	card[i].addEventListener('click', cardClick);
+	}
+    
 });
